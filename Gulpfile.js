a9b4db7d8ea181bcb46a0bfe303ed57714ba90ce
Erwan Osouf <erwan.osouf@gmail.com>
2017-03-25 15:26:12 +0100

    Update package / build manager

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var include = require('gulp-file-include');
 var gutil = require('gulp-util');
+var changed = require('gulp-changed');
+var serve = require('gulp-serve');
 
 
 var DEST = './build/';
@@ -14,8 +16,32 @@ gulp.task('includes', function() {
     .pipe(gulp.dest(DEST));
 });
 
+gulp.task('static', ['style', 'js', 'docs']);
+
+gulp.task('style', function() {
+    var dest = DEST + 'style';
+    gulp.src('./style/**/*').pipe(changed(dest)).pipe(gulp.dest(dest));
+});
+
+gulp.task('js', function() {
+    var dest = DEST + 'js';
+    gulp.src('./js/**/*').pipe(changed(dest)).pipe(gulp.dest(dest));
+});
+
+gulp.task('docs', function() {
+    var dest = DEST + 'docs';
+    gulp.src('./docs/**/*').pipe(changed(dest)).pipe(gulp.dest(dest));
+});
+
+gulp.task('build', ['static', 'includes']);
+
+gulp.task('serve', serve('build'));
+
 //Watch task
-gulp.task('default',function() {
+gulp.task('watch',function() {
     gulp.watch('./fragments/*.html', ['includes']);
     gulp.watch('./pages/*.html', ['includes']);
 });
+
+//Watch task
+gulp.task('default', ['build', 'watch', 'serve']);
