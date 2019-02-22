9cf100755616469615b5b269f5003987459b3456
Erwan Osouf <erwan.osouf@bellevillecitoyenne.fr>
2019-02-22 15:59:48 +0100

    Add Experiences

--- js/components/experience.js
+++ js/components/experience.js
@@ -1,3 +1,17 @@
 Vue.component('experience', {
-
+    template : `<div class="experience">
+        <span>{{experience.title }}</span>
+        <span>{{experience.company.name }}, {{experience.company.location }}</span>
+        <p v-if="experience.description">{{experience.description }}</p>
+        <ul>
+            <li v-for="project in projects">
+                <h5>{{ project.label }}</h5>
+                <span v-if="project.description">{{project.description}}</span> 
+            </li>
+        </ul>
+        <div>
+            <slot></slot>
+        </div>
+    </div>`,
+    props : [ 'experience' ]
 });
--- js/main.js
+++ js/main.js
@@ -76,6 +76,49 @@ var data = {
 			{ name : "Créole Guadeloupéen", level : "parlé" }
         ]
     }],
+    experiences : [{
+        title : "Ingénieur Études et Développements",
+        company : {
+            name : "Praxedo",
+            location : "Paris"
+        },
+        description : "Plate-forme SaaS de Field Service Management (Gestion d’interventions)",
+        date : "2013 - 2017",
+        duration : "4 ans",
+        projects : [{
+            label : "Mise en place d’interfaces avec plusieurs prestataires de géolocalisation",
+            description : `Intégration de flux de géolocalisation des techniciens via WebServices (TomTom
+            WebFleet, Orange Fleet et FleetFreedom) utilisant une infrastructure commune
+            pour le traitement des données.`
+        }, { 
+            label :"Développement d’une application web d’optimisation de la planification",
+            description : `Création d'une API REST exponsant les algorithmes d’optimisation développés lors de mon stage.
+            Intégration de la plate-forme avec cette application.`
+        }, { 
+            label :"Développement d’outils d’aide à la planification des interventions",
+            description : `Visualisations d’un planning : Gantt jour et semaine, planning technicien.
+                Module de suivi en temps réel et indicateurs de charge et d’avancement.
+                Module de visualisation géographique des interventions.`
+        }, { 
+            label : "Refonte de l’interface graphique du client de messagerie instantanée intégré"
+        }, { 
+            label :"Support technique applicatif"
+        }, { 
+            label :"Correction de bugs / Gestion des évolutions" 
+        }]
+    }, { 
+        title : "Projet de Fin d’Etudes", 
+        company : {
+            name : "Praxedo",
+            location : "Paris"
+        },
+        date : "2013",
+        duration : "6 mois",
+        projects : [
+            "Spécifications et développement d’algorithmes pour l’optimisation de tournées de techniciens sous contraintes.",
+            "Benchmarks, tests unitaires et documentation."
+        ]
+    }],
     formations : [{ 
         name : "École des Mines de Nantes (2009 – 2013)",
         description : " Formation Ingénieur, Option Génie Informatique pour l’Aide à la Décision"
